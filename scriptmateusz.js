ba392e98e056dfd90c3b85037f6b0f653889a1af
mateusz <matinow@poczta.onet.pl>
2023-11-12 20:05:37 +0100

    pop out

---
+++ scriptmateusz.js
@@ -0,0 +1,9 @@
+console.log('test')
+function showEkwipunek(){
+    console.log('przycisk')
+}
+
+function  showEkwipunek(){
+    let window_content = document.getElementById("EKWIPUNEK");
+    window_content.classList.toggle("show");
+}
