cd20d5588bc410a1d83534903253e4a9ef46cf04
Mati <matinow@poczta.onet.pl>
2023-12-09 04:05:39 +0100

    praca nad tabelką

--- scriptmateusz.js
+++ scriptmateusz.js
@@ -11,6 +11,26 @@ function  showEkwipunek(){
 
 const arr = ["nazwa","opis","działanie","typ","waga"]
 
+function dodajwiersz(tabelka) {
+    tabelka = document.getElementById("tabelka");
+    var NowyWiersz = tabelka.insertRow();
+       komórka1 = NowyWiersz.insertCell(0);
+       komórka2 = NowyWiersz.insertCell(1);
+       komórka3 = NowyWiersz.insertCell(2);
+       komórka4 = NowyWiersz.insertCell(3);
+       komórka5 = NowyWiersz.insertCell(4);    
+    }
 
+var tr = require('tr');
+
+function usunwiersz1(tabelka) {
+    tabelka = document.getElementById("tabelka");
+    var Usunwiersz = document.remove('tr');
+    komórka1 = Usunwiersz.removeCell(0);
+    komórka2 = Usunwiersz.removeCell(1);
+    komórka3 = Usunwiersz.removeCell(2);
+    komórka4 = Usunwiersz.removeCell(3);
+    komórka5 = Usunwiersz.removeCell(4);
+}
 
 
