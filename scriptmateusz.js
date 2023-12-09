c5d0d2d2f6c1018b52a2a6b9a8646bd809e30ce3
Mati <matinow@poczta.onet.pl>
2023-12-09 18:31:19 +0100

    usuwanie tabeli

--- scriptmateusz.js
+++ scriptmateusz.js
@@ -10,28 +10,46 @@ function  showEkwipunek(){
 
 
 const arr = ["Nazwa","Opis","Działanie","Typ","Waga"]
+let jestTrybUsuwania = false
+
+let tableRows = document.getElementsByTagName('tr')
+
+for(let row of tableRows){
+    row.addEventListener('click', usunwiersz);
+}
+
 
 function dodajwiersz() {
     tabelka = document.getElementById("tabelka");
-    var NowyWiersz = tabelka.insertRow();
-       komórka1 = NowyWiersz.insertCell(0);
-       komórka1 = document.createAttribute('Nazwa');
-       komórka2 = NowyWiersz.insertCell(1);
-       komórka2 = document.createAttribute('Opis');
-       komórka3 = NowyWiersz.insertCell(2);
-       komórka3 = document.createAttribute('Działanie');
-       komórka4 = NowyWiersz.insertCell(3);
-       komórka4 = document.createAttribute('Typ');
-       komórka5 = NowyWiersz.insertCell(4);  
-       komórka5 = document.createAttribute('Waga');  
+    let NowyWiersz = tabelka.insertRow();
+    for(let i = 0; i < 5; i++){
+        NowyWiersz.insertCell(i)
     }
 
-function usunwiersz1() {
     
-    if (NowyWiersz) {
-       let NowyWiersz = "tabelka" - "Nazwa" - "Opis" - "Działanie" - "typ" - "waga"
-    }
-    else{
-        null
-    }
+
+    // komórka1 = NowyWiersz.insertCell(0);
+    // // komórka1 = document.createAttribute('Nazwa');
+    // komórka2 = NowyWiersz.insertCell(1);
+    // // komórka2 = document.createAttribute('Opis');
+    // komórka3 = NowyWiersz.insertCell(2);
+    // // komórka3 = document.createAttribute('Działanie');
+    // komórka4 = NowyWiersz.insertCell(3);
+    // // komórka4 = document.createAttribute('Typ');
+    // komórka5 = NowyWiersz.insertCell(4);  
+    // komórka5 = document.createAttribute('Waga'); 
+
+    NowyWiersz.addEventListener('click', usunwiersz);
+}
+
+
+
+function usunwiersz(event) {
+    if(!jestTrybUsuwania) return
+    event.currentTarget.remove()
+}
+
+
+function zmianaTrybuUsuwania(){
+    jestTrybUsuwania = !jestTrybUsuwania;
 }
