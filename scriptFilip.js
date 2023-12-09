2f2c23572ce358dce0181df78465509579ac972b
Sury1920 <filip.szpulecki@gmail.com>
2023-12-09 22:38:12 +0100

    change class component

--- scriptFilip.js
+++ scriptFilip.js
@@ -3,5 +3,21 @@ function showClassChangePopup(){
 }
 
 function saveClassChange(){
+    let temp = document.getElementById('selectSecondaryClassChoice')
+
+    if(!temp.value) {temp.style.borderColor = 'red'; return;}
+
+    temp.style.borderColor = 'black';
     showClassChangePopup();
+}
+
+function handleRollUpClick(rollUpId){
+    document.getElementById(rollUpId).classList.toggle('show');
+}
+
+function handleClassOptionSelect(selectedOption){
+    document.getElementsByClassName('classOptionBox-selected')[0]?.classList.remove('classOptionBox-selected')
+    selectedOption.classList.add('classOptionBox-selected')
+
+    document.getElementById('classInfoHolder').innerText = 'Lorem Ipsum'
 }
