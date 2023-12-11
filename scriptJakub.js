48f7ab38d262f43d9d5b46b0de36fc99b5c51cc0
iscured <jakubex7503@gamil.com>
2023-12-11 23:17:16 +0100

    opis i bonusy

---
+++ scriptJakub.js
@@ -0,0 +1,8 @@
+let listaUmiejetnosci = [
+    { name: 'listwa starego', damage: 'instaKill', type: 'Brutality', cooldown: 3},
+    { name: 'luj ogłuszacz', damage: '2k8', type: 'ogłusza no', cooldown: 1},
+    { name: 'sratatat', damage: '3k4', type: 'tatatatarsa', cooldown: 10}
+]
+
+
+console.log("test")
