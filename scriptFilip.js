13b94bfab5119b6a7c3a62c48494cca99f7bd4ad
Sury1920 <filip.szpulecki@gmail.com>
2023-12-11 21:59:52 +0100

    listaitemkow

--- scriptFilip.js
+++ scriptFilip.js
@@ -21,3 +21,14 @@ function handleClassOptionSelect(selectedOption){
 
     document.getElementById('classInfoHolder').innerText = 'Lorem Ipsum'
 }
+
+
+
+
+
+
+let listaUmiejetnosci = [
+    { name: 'listwa starego', damage: 'instaKill', type: 'Brutality', cooldown: 3},
+    { name: 'luj ogłuszacz', damage: '2k8', type: 'ogłusza no', cooldown: 1},
+    { name: 'sratatat', damage: '3k4', type: 'tatatatarsa', cooldown: 10}
+]
