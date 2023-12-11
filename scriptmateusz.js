5340b4345505a70ff6f828621ac94e8e860d3e94
mateusz <matinow@poczta.onet.pl>
2023-12-11 23:17:04 +0100

    nic kurwa nie zrobiłem

--- scriptmateusz.js
+++ scriptmateusz.js
@@ -12,7 +12,7 @@ function  showEkwipunek(){
 const arr = ["Nazwa","Opis","Działanie","Typ","Waga"]
 let jestTrybUsuwania = false
 
-let tableRows = document.getElementsByTagName('tr')
+let tableRows = document.querySelectorAll('#tabelka tr')
 
 for(let row of tableRows){
     row.addEventListener('click', usunwiersz);
@@ -21,13 +21,21 @@ for(let row of tableRows){
 
 function dodajwiersz() {
     tabelka = document.getElementById("tabelka");
-    let NowyWiersz = tabelka.insertRow();
-    for(let i = 0; i < 6; i++){
-        NowyWiersz.insertCell(i)
-    }
-    NowyWiersz.addEventListener('click', usunwiersz);
+    tabelka2 = document.getElementById('dodanieprzedmiotu');   
+    tabelka.style.display = tabelka.style.display == "none" ? 'block' : 'none' ;
+    tabelka2.style.display = tabelka2.style.display == "block" ? 'none' : 'block' ;
+    // let NowyWiersz = tabelka.insertRow();
+    // for(let i = 0; i < 6; i++){
+    //     NowyWiersz.insertCell(i)
+    // }
+    // NowyWiersz.addEventListener('click', usunwiersz);
 }
 
+function przedmiotevent(event) {
+    tabelka2 = document.getElementById('dodanieprzedmiotu');
+    if(tabelka2.style.display == "block") return 
+    // event.currentTarget.
+}
 
 function usunwiersz(event) {
     if(!jestTrybUsuwania) return
